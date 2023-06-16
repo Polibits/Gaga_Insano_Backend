a241f6a83f7a624a602a1402db7b7ccc466cff88
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-06-15 23:15:08 -0300

    :books: protótipo de rotas de usuários

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,9 +2,29 @@ import express from 'express'
 const router = express.Router();
 import UserController from '../controllers/UserController';
 
+router.post('/user/create' , UserController.createUser);
+router.post('/user/login' , UserController.loginUser)
 
-router.post('/create' , UserController.createUser);
-router.post('/login' , UserController.loginUser)
+/* rotas de criação */
+// router.post('/user/create/default', UserController.createStudent)
+// router.post('/user/create/adm', UserController.createADM)
+// router.post('/user/create/tech', UserController.createTech)
 
+/* rotas de atualização */
+// router.post('/user/update/info', UserController.updateInfo)
+// router.post('/user/update/credentials', UserController.updateCredentials)
+// router.post('/user/update/activate', UserController.activateAccount)
+
+/* rotas de deleção */
+// router.post('/user/delete', UserController.deleteUser)
+
+/* rotas de leitura */
+// router.get('/user/read/info/all', UserController.getAllUsersInfo)
+// router.get('/user/read/info/one', UserController.getUserInfo)
+
+/* rotas de bloqueio */
+// router.post('/user/block/newblock', UserController.blockUser)
+// router.post('/user/block/unblock', UserController.unblockUser)
+// router.post('/user/block/status', UserController.isBlocked)
 
 export default router;
