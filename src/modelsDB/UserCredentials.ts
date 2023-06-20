4e7d0b7b3674b22eac33f68347583ca5658ad97e
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-06-20 17:15:08 -0300

    :hammer: redefinir UserCredentials no DB

--- src/modelsDB/UserCredentials.ts
+++ src/modelsDB/UserCredentials.ts
@@ -1,30 +1,21 @@
 import Sequelize, { Model } from "sequelize";
 import sequelize from '../db/conn';
-import Auth from "../auth/auth";
 
 /**
  * Define modelo das credenciais do usuário, no banco de dados
  */
 export const UserCredentials = sequelize.define('userCredentials', {
-    userID: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        defaultValue: () => {
-            return Auth.newUserId(); // Chame uma função para gerar o userID aqui
-        }
-    },
-    email: {
+    hashedEmail: {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true
     },
-    salt: {
+    hashedPassword: {
         type: Sequelize.STRING,
         allowNull: false
     },
-    password: {
+    salt: {
         type: Sequelize.STRING,
         allowNull: false
-    } 
+    }
 });
